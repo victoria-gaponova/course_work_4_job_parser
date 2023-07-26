2d4b9ad2c4d6d69093fcbd586a500ca2437784ba
Victoria Gaponova <victoria.gaponava@gmail.com>
2023-07-27 02:33:31 +0300

    Implement BaseAPI class with abstract get_vacancies() method

---
+++ api/base_api.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+class BaseApi(ABC):
+    """Абстрактный класс для работы с API сайтов с вакансиями.
+
+    Атрибуты:
+        number_of_vacancies (int): Количество вакансий, которое нужно получить.
+
+    Методы:
+        __init__(self, number_of_vacancies: int = 100):
+            Конструктор абстрактного класса для инициализации количества вакансий.
+
+        get_vacancies(self, vacancy: str) -> List[Vacancy]:
+            Абстрактный метод для получения списка вакансий по заданной должности.
+
+    """
+
+    def __init__(self, number_of_vacancies: int = 100):
+        """Инициализирует атрибуты объекта BaseApi.
+
+        Аргументы:
+            number_of_vacancies (int): Количество вакансий, которое нужно получить. По умолчанию 100.
+        """
+        self.number_of_vacancies = number_of_vacancies
+
+    @abstractmethod
+    def get_vacancies(self, vacancy: str) -> List[Vacancy]:
+        """Абстрактный метод для получения списка вакансий по заданной должности.
+
+        Аргументы:
+            vacancy (str): Должность, по которой нужно получить список вакансий.
+
+        Возвращает:
+            List[Vacancy]: Список объектов вакансий, полученных из API сайта.
+        """
+        pass
